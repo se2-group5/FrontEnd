18e7259ecedcb4d22967ec034fcedd64c93cec8d
drriosg <riosgadavid@gmail.com>
2022-07-01 13:34:10 -0500

    AboutUs done

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -4,48 +4,99 @@ import "./AboutUs.css";
 function AboutUs() {
   return (
     <div className="aboutus">
-      <div className="aboutus">
-        <h1 className="aboutus__tittle">Acerca de Nosotros</h1>
-        <div className="aboutus__container">
-          <p>
-            DIG nace cómo una iniciativa ante el aumento del teletrabajo y la creciente población de 
-            nómadas digitales. DIG brinda información de lugares de coworking, ya bien puedan ser lugares como cafés con
-            espacios adapatados para coworking o lugares enteramente diseñados para ello. Además de brindar información de horario y
-            ubicación de Coworking, ofrecemos la posibilidad de que los mismos usuarios reporten su experiencia y actualicen el estados de
-            los sitios que frecuentan en tiempo real para así poder brindar información más precisa, actualizada y oportuna. 
+      <div className="aboutus__container">
+        <div className="content">
+          <h1 className="aboutus__tittle">Acerca de Nosotros</h1>
+          <p className="content__p">
+            DIG nace cómo una iniciativa ante el aumento del teletrabajo y la
+            creciente población de nómadas digitales. DIG brinda información de
+            lugares de coworking, ya bien puedan ser lugares como cafés con
+            espacios adapatados para coworking o lugares enteramente diseñados
+            para ello. Además de brindar información de horario y ubicación de
+            Coworking, ofrecemos la posibilidad de que los mismos usuarios
+            reporten su experiencia y actualicen el estados de los sitios que
+            frecuentan en tiempo real para así poder brindar información más
+            precisa, actualizada y oportuna.
           </p>
         </div>
-        <div className="aboutus__container">
-          <h3>Financiamiento</h3>
-          <p>
-            DIG no cobra a los usuarios por el uso de la aplicación, ni a los establecimientos por aparecer en nuestra web.
-            Para mantener esta iniciativa, fomentamos que nuestros usuarios puedan donar para así nosotros poder mantener y mejorar
-            la aplicación. Con tus aportes ayudas a mantener nuestra comunidad activa y en expansión. 
+      </div>
+      <div className="aboutus__container">
+        <div className="content">
+          <h3 className="aboutus__tittle">Financiamiento</h3>
+          <p className="content__p">
+            DIG no cobra a los usuarios por el uso de la aplicación, ni a los
+            establecimientos por aparecer en nuestra web. Para mantener esta
+            iniciativa, fomentamos que nuestros usuarios puedan donar para así
+            nosotros poder mantener y mejorar la aplicación. Con tus aportes
+            ayudas a mantener nuestra comunidad activa y en expansión.
           </p>
         </div>
-        <div className="aboutus__container">
-          <h3>Información legal</h3>
-          <p>
-            Toda la información suministrada, compartida y expuesta en medios presenciales, digitales, impresos son de uso exclusivo de DIG en cuanto a información propia de su plataforma.
-            De acuerdo a la ley 44  de 1993 sobre los derechos de autor, todo el desarrollo realizado tanto material físico como virtual (software) están protegidos y patentados. Por ende el contenido de los repositorios de código fuente son de uso exclusivo de DIG y se prohibe a sus miembros o terceros la distribución u extracción total o parcial del código fuente, así como el uso de logo, paleta de color y patrones de diseño de la aplicación.
-            DIG se rige bajo la normatividad colombiana consiganada en la constitución política colombiana de 1991, las leyes y normativas expedidas por la rama legislativa, la superintendencia de industria y comercio, la dirección de impuestos y aduanas nacionales y la cámara de comercio.
-            Dado el caso que las autoridades requieran información de DIG bajo ordenes judiciales y acatando los fallos o sentencias puestas por la rama judicial se realizarán acorde a la ley.
-            <div>
-              <h5>Información legal para usuarios</h5>
-              De acuerdo con el decreto 1377 de 2013, la ley 527 de 1999 y el Habeas Data, sus datos son recolectados tratados, protegidos y dispuestos como lo dispone la ley.
-              Tenga en cuenta que los datos que registre en DIG serán tratados con el fin del desarrollo de estadísticas y funciones de las aplicación, protegiendo siempre sus datos sensibles. 
-              Tendrá siempre la disponibilidad de consultar sus datos personales y poder solicitar su actualización o eliminación de estos. 
-              Tenga en cuenta que al momento de crear su cuenta en DIG, sus datos serán almacenados en una base de datos reservada y protegida mientras tenga su cuenta activa, una vez solicite y se realice la eliminación de su cuenta, DIG no almacenará ningún tipo de información en sus bases de datos y ni tanto DIG cómo el usuario podrán recuperar la información de su cuenta. 
-              Tenga en cuenta que sus datos podrán ser compartidos a terceros según lo establezca la política de datos. Así como también a las autoridades judiciales  en caso de que dichas instituciones lo requieran.
-              Usted como usuario/establecimiento acepta y asegura que la información suministrada es verídica, verás y el uso de la aplicación lo hará siempre bajo legalidad.
-              La información suministrada por los usuarios y establecimientos esta sujeta a verificación, por tanto puede ser eliminada o modificada bajo acuerdo previo en caso de poseer información falsa o que viole nuestras políticas.
-              DIG es un medio por el cual brinda información a los establecimientos, por lo tanto, DIG no es reponsable de cualquier tipo de daño, perjuicio, altercado o situación presentada en algún establecimiento.
-              DIG no es un canal de trámites o recepción de solicitudes para establecimientos.
-              DIG no compartirá información de sus usuarios para los establecimientos, todas las estadísticas generadas serán de caracter anónimo, bajo excepción de los reportes. Bajo ninún caso, DIG proporcioanrá más información de usuarios a los establecimientos o a terceros, bajo excepciones de ley.
-              <h5>Información legal para la Universidad Nacional</h5>
-              Tenga en cuenta que toda la documentación e información desplegada por DIG solamente será usada para uso interno y para divulgación con propósitos académicos en la Universidad Nacional de Colombia sede Bogotá en el curso de Ingeniería de Software 2 únicamente durante el periodo de vigencia del semestre 2022-1 conforme a lo establecido en el calendario académico expedido por la universidad. Se prohíbe todo tipo de divulgación total o parcial a terceros.
-              DIG no posee ningún tipo de relación contractual o vinculativa, ni representa la Universidad Nacional de Colombia o alguna de sus dependencias.
-            </div>
+      </div>
+      <div className="aboutus__container">
+        <div className="content">
+          <h3 className="aboutus__tittle">Información legal</h3>
+          <p className="content__p">
+            Toda la información suministrada, compartida y expuesta en medios
+            presenciales, digitales, impresos son de uso exclusivo de DIG en
+            cuanto a información propia de su plataforma. De acuerdo a la ley 44
+            de 1993 sobre los derechos de autor, todo el desarrollo realizado
+            tanto material físico como virtual (software) están protegidos y
+            patentados. Por ende el contenido de los repositorios de código
+            fuente son de uso exclusivo de DIG y se prohibe a sus miembros o
+            terceros la distribución u extracción total o parcial del código
+            fuente, así como el uso de logo, paleta de color y patrones de
+            diseño de la aplicación. DIG se rige bajo la normatividad colombiana
+            consiganada en la constitución política colombiana de 1991, las
+            leyes y normativas expedidas por la rama legislativa, la
+            superintendencia de industria y comercio, la dirección de impuestos
+            y aduanas nacionales y la cámara de comercio. Dado el caso que las
+            autoridades requieran información de DIG bajo ordenes judiciales y
+            acatando los fallos o sentencias puestas por la rama judicial se
+            realizarán acorde a la ley.
+            <h5 className="aboutus__tittle">Información legal para usuarios</h5>
+            De acuerdo con el decreto 1377 de 2013, la ley 527 de 1999 y el
+            Habeas Data, sus datos son recolectados tratados, protegidos y
+            dispuestos como lo dispone la ley. Tenga en cuenta que los datos que
+            registre en DIG serán tratados con el fin del desarrollo de
+            estadísticas y funciones de las aplicación, protegiendo siempre sus
+            datos sensibles. Tendrá siempre la disponibilidad de consultar sus
+            datos personales y poder solicitar su actualización o eliminación de
+            estos. Tenga en cuenta que al momento de crear su cuenta en DIG, sus
+            datos serán almacenados en una base de datos reservada y protegida
+            mientras tenga su cuenta activa, una vez solicite y se realice la
+            eliminación de su cuenta, DIG no almacenará ningún tipo de
+            información en sus bases de datos y ni tanto DIG cómo el usuario
+            podrán recuperar la información de su cuenta. Tenga en cuenta que
+            sus datos podrán ser compartidos a terceros según lo establezca la
+            política de datos. Así como también a las autoridades judiciales en
+            caso de que dichas instituciones lo requieran. Usted como
+            usuario/establecimiento acepta y asegura que la información
+            suministrada es verídica, verás y el uso de la aplicación lo hará
+            siempre bajo legalidad. La información suministrada por los usuarios
+            y establecimientos esta sujeta a verificación, por tanto puede ser
+            eliminada o modificada bajo acuerdo previo en caso de poseer
+            información falsa o que viole nuestras políticas. DIG es un medio
+            por el cual brinda información a los establecimientos, por lo tanto,
+            DIG no es reponsable de cualquier tipo de daño, perjuicio, altercado
+            o situación presentada en algún establecimiento. DIG no es un canal
+            de trámites o recepción de solicitudes para establecimientos. DIG no
+            compartirá información de sus usuarios para los establecimientos,
+            todas las estadísticas generadas serán de caracter anónimo, bajo
+            excepción de los reportes. Bajo ninún caso, DIG proporcioanrá más
+            información de usuarios a los establecimientos o a terceros, bajo
+            excepciones de ley.
+            <h5 className="aboutus__tittle">
+              Información legal para la Universidad Nacional
+            </h5>
+            Tenga en cuenta que toda la documentación e información desplegada
+            por DIG solamente será usada para uso interno y para divulgación con
+            propósitos académicos en la Universidad Nacional de Colombia sede
+            Bogotá en el curso de Ingeniería de Software 2 únicamente durante el
+            periodo de vigencia del semestre 2022-1 conforme a lo establecido en
+            el calendario académico expedido por la universidad. Se prohíbe todo
+            tipo de divulgación total o parcial a terceros. DIG no posee ningún
+            tipo de relación contractual o vinculativa, ni representa la
+            Universidad Nacional de Colombia o alguna de sus dependencias.
           </p>
         </div>
       </div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,25 +9,8 @@ function Footer() {
   return (
     <div className="footer">
       <div className="footer__container">
-        <h3 className="footer__logo">© Copyright 2022</h3>
-        <ul className="footer__list">
-          <li className="footer__item">
-            <Link className="footer__link" to="/aboutus">
-              Acerca de Nosotros
-            </Link>
-          </li>
-          <li className="footer__item">
-            <Link className="footer__link" to="/donations">
-              Donaciones
-            </Link>
-          </li>
-          <li className="footer__item">
-            <Link className="footer__link" to="/donations">
-              Privacidad
-            </Link>
-          </li>
-        </ul>
         <div className="footer__social">
+          <span className="footer__text">Nuestras Redes</span>
           <a
             href="https://web.facebook.com/DIG-APP-105427112229134"
             target="_blank"
@@ -39,9 +22,21 @@ function Footer() {
             <FaInstagram className="icon" />
           </a>
         </div>
-      </div>
-      <div className="footer__container2">
-        <h3 className="footer__logo__2">DIG APP</h3>
+
+        <ul className="footer__list">
+          <li className="footer__item">
+            <Link className="footer__link" to="/aboutus">
+              Acerca de Nosotros
+            </Link>
+          </li>
+          <li className="footer__item">
+            <Link className="footer__link" to="/donations">
+              Donaciones
+            </Link>
+          </li>
+          <li className="footer__item"></li>
+        </ul>
+        <h3 className="footer__logo">© DIG APP</h3>
       </div>
     </div>
   );
