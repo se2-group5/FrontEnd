6e6a64b75af67681e4d32e501d7ef5d4ee284815
drriosg <riosgadavid@gmail.com>
2022-07-01 15:27:18 -0500

    Fix Form Report

--- src/Pages/Establishment/MakeReport/MakeReport.jsx
+++ src/Pages/Establishment/MakeReport/MakeReport.jsx
@@ -44,6 +44,7 @@ function MakeReport() {
     } else if (name === "internet_status") {
       setInternet_status(value);
     } else if (name === "occupation_status") {
+      console.log(name, value);
       setOccupation_status(value);
     } else if (name === "comments") {
       setComments(value);
@@ -55,30 +56,45 @@ function MakeReport() {
       <h3 className="make__tittle">Reporta</h3>
       <div className="make__form__container">
         <form className="make__form">
-          <input
-            type="number"
-            placeholder="Calificación del lugar"
+          <span className="make__text">Calificación del lugar</span>
+          <select
             className="make__input"
             name="rating_business"
             onChange={(e) => handleChange(e.target.name, e.target.value)}
-          />
-          <input
-            type="number"
-            placeholder="Velocidad Internet"
+          >
+            <option value={0}>0</option>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={4}>5</option>
+          </select>
+          <span className="make__text">Velocidad Internet</span>
+          <select
             className="make__input"
             name="internet_status"
             onChange={(e) => handleChange(e.target.name, e.target.value)}
-          />
-          <input
-            type="number"
-            placeholder="Ocupación"
+          >
+            <option value={0}>0</option>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={4}>5</option>
+          </select>
+          <span className="make__text">Ocupación</span>
+          <select
             className="make__input"
             name="occupation_status"
             onChange={(e) => handleChange(e.target.name, e.target.value)}
-          />
+          >
+            <option value={1}>Vacío</option>
+            <option value={2}>Intermedio</option>
+            <option value={3}>LLeno</option>
+          </select>
+          <span className="make__text">Cometarios</span>
           <input
             type="textarea"
-            placeholder="Comentarios"
             className="make__input make__text__area"
             name="comments"
             onChange={(e) => handleChange(e.target.name, e.target.value)}
